88868702f0866d0f5506228f831b87960f118d69
Pathum Dilhara <hbgpathumdilhara@gmail.com>
2025-08-18 19:15:54 +0530

    make responsive

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import './Navbar.css'
 import AnchorLink from 'react-anchor-link-smooth-scroll';
+import close from '../../assets/menu_close.svg';
+import open from '../../assets/menu_open.svg'
+
 
 const Navbar = () => {
+    
+    const menuRef = useRef();
+
+    const openMenu = () => {
+        menuRef.current.style.right="0";
+    }
+
+    const closeMenu = () => {
+        menuRef.current.style.right="-350px";
+    }
+
     return (
         <div className='navbar'>
             <h1>Portfolio</h1>
-            <ul className='nav-menu'>
+            <img src={open} onClick={openMenu} className='nav-menu-open' alt="" />
+            <ul ref={menuRef} className='nav-menu'>
+                <img src={close} onClick={closeMenu} className='nav-menu-close' alt="" />
                 <li><AnchorLink class='anchor-link'  href='#home'>Home</AnchorLink></li>
                 <li><AnchorLink class='anchor-link'  href='#education'>Education</AnchorLink></li>
                 <li><AnchorLink class='anchor-link'  href='#tools'>Tools</AnchorLink></li>
@@ -16,7 +32,6 @@ const Navbar = () => {
             <AnchorLink class='anchor-link'  href='#contact'>
                 <div className="nav-connect">Contact Me</div>
             </AnchorLink>
-            
         </div>
     );
 }
